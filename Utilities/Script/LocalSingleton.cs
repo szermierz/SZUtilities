07dcd2dcd5f974c5aea3a91ccdb2b4947920458e
Kamil Kowalewski <kamil.kowalewski@engineer.com>
2022-04-23 19:23:27 +0200

    [Script] Added LocalSingleton

---
+++ Utilities/Script/LocalSingleton.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace SZUtilities
+{
+    [DefaultExecutionOrder(-999)]
+    public abstract class LocalSingleton<SingletonType> : MonoBehaviourEx
+        where SingletonType : LocalSingleton<SingletonType>
+    {
+        #region Singleton
+
+        protected virtual bool WaitForInitialize => true;
+
+        private static LocalSingleton<SingletonType> s_earlyInstance = null;
+        public static SingletonType Instance { get; private set; }
+
+        public bool Initialized { get; private set; }
+
+        protected virtual bool MoveToDontDestroy => false;
+
+        private void InitializeSingleton()
+        {
+            if (s_earlyInstance)
+            {
+                Destroy(gameObject);
+                return;
+            }
+
+            s_earlyInstance = this;
+
+            if (!WaitForInitialize)
+                Instance = this as SingletonType;
+
+            if(MoveToDontDestroy)
+                DontDestroyOnLoad(gameObject);
+
+            var init = Initialize();
+            if (null != init)
+            {
+                init = Routines.Concat(init, Routines.Action(() =>
+                {
+                    Instance = this as SingletonType;
+                    Initialized = true;
+                }));
+                StartCoroutine(init);
+            }
+            else
+            {
+                Instance = this as SingletonType;
+                Initialized = true;
+            }
+        }
+
+        #endregion
+
+        protected sealed override void Awake()
+        {
+            base.Awake();
+
+            InitializeSingleton();
+        }
+
+        protected virtual IEnumerator Initialize() => null;
+    }
+}
--- Utilities/Script/Singleton.cs
+++ Utilities/Script/Singleton.cs
@@ -1,62 +1,11 @@
-﻿using System.Collections;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace SZUtilities
 {
     [DefaultExecutionOrder(-999)]
-    public class Singleton<SingletonType> : MonoBehaviourEx
+    public class Singleton<SingletonType> : LocalSingleton<SingletonType>
         where SingletonType : Singleton<SingletonType>
     {
-        #region Singleton
-
-        protected virtual bool WaitForInitialize => true;
-
-        private static Singleton<SingletonType> s_earlyInstance = null;
-        public static SingletonType Instance { get; private set; }
-
-        public bool Initialized { get; private set; }
-
-        private void InitializeSingleton()
-        {
-            if (s_earlyInstance)
-            {
-                Destroy(gameObject);
-                return;
-            }
-
-            s_earlyInstance = this;
-
-            if (!WaitForInitialize)
-                Instance = this as SingletonType;
-
-            DontDestroyOnLoad(gameObject);
-
-            var init = Initialize();
-            if (null != init)
-            {
-                init = Routines.Concat(init, Routines.Action(() =>
-                {
-                    Instance = this as SingletonType;
-                    Initialized = true;
-                }));
-                StartCoroutine(init);
-            }
-            else
-            {
-                Instance = this as SingletonType;
-                Initialized = true;
-            }
-        }
-
-        #endregion
-
-        protected sealed override void Awake()
-        {
-            base.Awake();
-
-            InitializeSingleton();
-        }
-
-        protected virtual IEnumerator Initialize() => null;
+        protected override bool MoveToDontDestroy => true;
     }
 }
