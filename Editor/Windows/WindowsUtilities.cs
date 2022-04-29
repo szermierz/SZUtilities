61fded0b9feef833c4550229833a5feba50bbc41
Kamil Kowalewski <kamil.kowalewski@engineer.com>
2022-04-29 11:16:15 +0200

    [Editor][Utilities] Added Windows utilities

---
+++ Editor/Windows/WindowsUtilities.cs
@@ -0,0 +1,11 @@
+﻿namespace SZUtilities._Editor.Windows
+{
+    public static class WindowsUtilities
+    {
+        public static void ShowDirectoryInExplorer(string directory)
+        {
+            directory = directory.Replace(@"/", @"\");   // explorer doesn't like front slashes
+            System.Diagnostics.Process.Start("explorer.exe", $"/select,{directory}");
+        }
+    }
+}
