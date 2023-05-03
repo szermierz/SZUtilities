ce3c69a891b2ead15f590c50b08e1ade70156423
Kamil Kowalewski <kamil.kowalewski@engineer.com>
2023-05-03 10:13:09 +0200

    [Scripts] Added cancellables

---
+++ Utilities/Script/CancelHandle.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using SZUtilities.Extensions;
+
+namespace SZUtilities.Cancellables
+{
+    public interface ICancellable
+    {
+        void Cancel(long cancellableIndex);
+    }
+
+    public static class CancelHandlePool
+    {
+        private static long s_cancellableIndex;
+        private static List<CancelHandle> m_freeHandles = new();
+
+        public static (IDisposable handle, long cancellableIndex) Rent(ICancellable cancellable)
+        {
+            lock(m_freeHandles)
+            {
+                if (!m_freeHandles.Any())
+                    m_freeHandles.Add(new CancelHandle());
+
+                var handle = m_freeHandles[0];
+                m_freeHandles.RemoveUnorderedAt(0);
+
+                handle.Cancellable = cancellable;
+                handle.CancellableIndex = ++s_cancellableIndex;
+
+                return (handle, handle.CancellableIndex);
+            }
+        }
+
+        private class CancelHandle : IDisposable
+        {
+            public long CancellableIndex;
+            public ICancellable Cancellable;
+
+            public void Dispose()
+            {
+                lock(m_freeHandles)
+                {
+                    Cancellable?.Cancel(CancellableIndex);
+                    Cancellable = null;
+                    m_freeHandles.Add(this);
+                }
+            }
+        }
+    }
+
+
+
+}
