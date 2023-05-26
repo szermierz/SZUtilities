6bcf9146a87c32a934ac76f0d6ec4a67dd6553de
Kamil Kowalewski <kamil.kowalewski@engineer.com>
2023-05-26 23:41:49 +0200

    [Extensions] Added remove list extensions

--- Extensions/TypesExtensions/ListExtensions.cs
+++ Extensions/TypesExtensions/ListExtensions.cs
@@ -1,17 +1,79 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 
 namespace SZUtilities.Extensions
 {
     public static class ListExtensions
     {
+        public static int Remove<T>(this List<T> list, IEnumerable<T> range)
+        {
+            var result = 0;
+
+            foreach (var item in range)
+            {
+                var index = list.IndexOf(item);
+                if (index < 0)
+                    continue;
+
+                list.RemoveAt(index);
+                ++result;
+            }
+
+            return result;
+        }
+        public static int Remove<T>(this List<T> list, IEnumerator<T> range)
+        {
+            var result = 0;
+
+            while(range.MoveNext())
+            {
+                var item = range.Current;
+                var index = list.IndexOf(item);
+                if (index < 0)
+                    continue;
+
+                list.RemoveAt(index);
+                ++result;
+            }
+
+            return result;
+        }
+
         public static bool RemoveUnordered<T>(this List<T> list, T value)
         {
             var index = list.IndexOf(value);
             if (index < 0)
                 return false;
-            
+
             RemoveUnorderedAt(list, index);
             return true;
+        }
+
+        public static int RemoveUnordered<T>(this List<T> list, IEnumerable<T> range)
+        {
+            var result = 0;
+
+            foreach (var item in range)
+            {
+                if (list.RemoveUnordered(item))
+                    ++result;
+            }
+
+            return result;
+        }
+
+        public static int RemoveUnordered<T>(this List<T> list, IEnumerator<T> range)
+        {
+            var result = 0;
+
+            while (range.MoveNext())
+            {
+                var item = range.Current;
+                if(list.RemoveUnordered(item))
+                    ++result;
+            }
+
+            return result;
         }
 
         public static void RemoveUnorderedAt<T>(this List<T> list, int index)
