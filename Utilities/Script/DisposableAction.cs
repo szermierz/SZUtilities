e068ee9df27aa57067e3e82f08ba7dcc7f4b60c0
Kamil Kowalewski <kamil.kowalewski@engineer.com>
2023-07-14 12:35:34 +0200

    [Utilities][Scripts] Pooled disposable action

---
+++ Utilities/Script/DisposableAction.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using SZUtilities.Extensions;
+
+namespace SZUtilities
+{
+    public sealed class DisposableAction : IDisposable
+    {
+        private static List<DisposableAction> s_actionsPool = new();
+
+        public static IDisposable Create(Action callback)
+        {
+            if (!s_actionsPool.Any())
+                s_actionsPool.Add(new DisposableAction());
+
+            var action = s_actionsPool.PopUnordered();
+            
+            if (null != action.m_callback)
+                throw new Exception();
+
+            action.m_callback = callback;
+            
+            return action;
+        }
+
+        private Action m_callback;
+
+        private DisposableAction()
+        { }
+
+        public void Dispose()
+        {
+            m_callback?.Invoke();
+            m_callback = null;
+
+            s_actionsPool.Add(this);
+        }
+    }
+}
+
